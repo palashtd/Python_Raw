939e1fb7c2cae1a8c1fe8565ed419ace56d79834
Palash <mpalashtd@gmail.com>
2023-09-21 11:19:33 +0600

    added thanks text only

---
+++ Handling_division_by_zero.py
@@ -0,0 +1,24 @@
+# Problem 1: Handling Division by Zero
+# Guidelines:
+# •	Write a Python program that takes two numbers as input from the user.
+# •	Use a try-except block to handle the division operation.
+# •	If the user tries to divide by zero, catch the exception and print an error message.
+
+try:
+    while True:
+        try:
+            user_num1 = float(input("Enter your number here: "))
+            user_num2 = float(input("Enter your number here: "))
+            result = user_num1 / user_num2
+            print(result)
+            if result:
+                break
+            else:
+                continue
+        except ZeroDivisionError:
+            print("Division by zero not allowed")
+except ValueError:
+    print('Please enter correct value')
+
+# Create by Palash Talukder.
+Thanks
