a5ae7011378f7534d9c6cb46c4f62e1ac666835f
Palash <mpalashtd@gmail.com>
2023-09-21 11:36:26 +0600

    Using guidelines added

--- Handling_division_by_zero.py
+++ Handling_division_by_zero.py
@@ -3,6 +3,7 @@
 # •	Write a Python program that takes two numbers as input from the user.
 # •	Use a try-except block to handle the division operation.
 # •	If the user tries to divide by zero, catch the exception and print an error message.
+# Only Python Raw Code. Use the below code on your code editor.
 
 try:
     while True:
